9677b538c33c732cb2e109c55b8db9ea8fdf0819
Raunak Bajaj <raunak.bajaj@senso.ai>
2020-03-05 21:47:57 -0500

    is_straight function

--- Poker.ex
+++ Poker.ex
@@ -1,31 +1,12 @@
 defmodule Poker do
 
-        def deal_hand_1([], res) do IO.inspect res, charlists: :as_lists end
-        def deal_hand_1(card_list) when length(card_list)==10  do deal_hand_1(card_list, []) end
-        def deal_hand_1(card_list, hand) do
-                if rem(length(card_list), 2)==0 do
-                        hand=hand++[hd(card_list)]                
-                        deal_hand_1(tl(card_list), hand)
-                else 
-                        deal_hand_1(tl(card_list), hand)
-                end
-        end
-        def deal_hand_1(_) do :error end
+	def is_straight(hand) do
+		hand_rem = Enum.sort(Enum.map(hand, fn(n) -> rem(n,13) end))
+		hand_rem==[0,1,10,11,12] or
+		hand_rem
+		|> Enum.chunk_every(2, 1, :discard)
+		|> Enum.map(fn [x, y] -> y - x end)==[1,1,1,1]
 
-        def deal_hand_2([], res) do res end
-        def deal_hand_2(card_list) when length(card_list)==10  do deal_hand_2(card_list, []) end
-        def deal_hand_2(card_list, hand) do
-                if rem(length(card_list), 2)==1 do
-                        hand=hand++[hd(card_list)]
-                        deal_hand_2(tl(card_list), hand)
-                else
-                        deal_hand_2(tl(card_list), hand)
-                end
-        end
-        def deal_hand_2(_) do :error end
+	end
 
-        def num_ocurrences(list) do
-                new_list = list |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
-                new_list
-        end
 end
