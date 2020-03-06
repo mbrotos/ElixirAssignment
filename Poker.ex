96c8686425463150bbd13aa36e8d914bfc3d592f
Raunak Bajaj <raunak.bajaj@senso.ai>
2020-03-05 22:00:45 -0500

    fixed or

--- Poker.ex
+++ Poker.ex
@@ -2,7 +2,7 @@ defmodule Poker do
 
 	def is_straight(hand) do
 		hand_rem = Enum.sort(Enum.map(hand, fn(n) -> rem(n,13) end))
-		hand_rem==[0,1,10,11,12] or
+		hand_rem==[0,1,10,11,12] ||
 		hand_rem
 		|> Enum.chunk_every(2, 1, :discard)
 		|> Enum.map(fn [x, y] -> y - x end)==[1,1,1,1]
