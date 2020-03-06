4d5b3eeed897e5ecb129f1ff3cd1582bea106df8
Adam Sorrenti <adam.sorrenti@ryerson.ca>
2020-03-05 21:55:02 -0500

    Straight added

--- Poker.ex
+++ Poker.ex
@@ -4,10 +4,12 @@ defmodule Poker do
     end
 
     def is_straight(hand) do
-        bHand = baseHand(hand)
-        length(Enum.uniq(bHand)) == 5 &&
-        Enum.uniq(bHand |> Enum.chunk_every(2, 1, :discard) |> Enum.map(fn [x, y] -> y-x end)) == [1] 
-    end
+		hand_rem = Enum.sort(Enum.map(hand, fn(n) -> rem(n,13) end))
+		hand_rem==[0,1,10,11,12] or
+		hand_rem
+		|> Enum.chunk_every(2, 1, :discard)
+		|> Enum.map(fn [x, y] -> y - x end)==[1,1,1,1]
+	end
 
     def is_fourOfKind(hand) do
         bHand = baseHand(hand) 
