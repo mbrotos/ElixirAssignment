8fb6ec0611d2ebed45fbc8412963fb7f920ca388
Raunak Bajaj <raunak.bajaj@senso.ai>
2020-03-06 00:00:36 -0500

    stuff

--- Poker.ex
+++ Poker.ex
@@ -1,12 +1,29 @@
 defmodule Poker do
 
-	def is_straight(hand) do
-		hand_rem = Enum.sort(Enum.map(hand, fn(n) -> rem(n,13) end))
-		hand_rem==[0,1,10,11,12] ||
-		hand_rem
-		|> Enum.chunk_every(2, 1, :discard)
-		|> Enum.map(fn [x, y] -> y - x end)==[1,1,1,1]
+	def find_n_OfKind(list,num) do
+		aceKingFn = fn
+			e1 when e1==0 or e1==1 -> e1+13
+			e1->e1
+		end
+		
+		list
+		|> Enum.map(aceKingFn)
+		|> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
+		|> Enum.find(fn {key, val} -> val == num  end) |> elem(0)
 
 	end
+
+
+	def tie_fourOfKind(hand1, hand2) do
+		bhand1 = Enum.map(hand1, fn(n) -> rem(n,13) end)
+		bhand2 = Enum.map(hand2, fn(n) -> rem(n,13) end)
+		find_n_OfKind(bhand1, 4) > find_n_OfKind(bhand2, 4)&& hand1 || find_n_OfKind(bhand1, 4) < find_n_OfKind(bhand2, 4)&& hand2
+	end
+
+	def tie_threeOfKind(hand1, hand2) do
+                bhand1 = Enum.map(hand1, fn(n) -> rem(n,13) end)
+                bhand2 = Enum.map(hand2, fn(n) -> rem(n,13) end)
+                find_n_OfKind(bhand1, 3) > find_n_OfKind(bhand2, 3)&& hand1 || find_n_OfKind(bhand1, 3) < find_n_OfKind(bhand2, 3)&& hand2
+        end
 
 end
