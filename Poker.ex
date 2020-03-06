13d1d359d5cd53b6cdc9899f901f2a97e72a080c
Adam Sorrenti <adam.sorrenti@ryerson.ca>
2020-03-06 18:05:37 -0500

    tie_pair

--- Poker.ex
+++ Poker.ex
@@ -4,27 +4,6 @@ defmodule Poker do
         Enum.map(hand, &(rem(&1,13))) 
     end
 
-    def is_royalFlush(hand) do
-        is_flush(hand) &&
-        is_straight(hand) &&
-        baseHand(hand) == [0,1,10,11,12]
-    end
-
-    def is_straightFlush(hand) do
-        is_flush(hand) &&
-        is_straight(hand)
-    end
-
-    def is_fourOfKind(hand) do
-        bHand = baseHand(hand) 
-        length(Enum.uniq(bHand)) == 2
-    end
-
-    def is_fullHouse(hand) do
-        is_threeOfKind(hand) &&
-        is_twoPair(hand)
-    end
-
     def is_flush(hand) do 
         Enum.all?(hand, &(&1 in 1..13))  ||
         Enum.all?(hand, &(&1 in 14..26)) ||
@@ -59,6 +38,27 @@ defmodule Poker do
         length(Enum.uniq(bHand)) == 4
     end
 
+    def is_royalFlush(hand) do
+        is_flush(hand) &&
+        is_straight(hand) &&
+        baseHand(hand) == [0,1,10,11,12]
+    end
+
+    def is_straightFlush(hand) do
+        is_flush(hand) &&
+        is_straight(hand)
+    end
+
+    def is_fourOfKind(hand) do
+        bHand = baseHand(hand) 
+        length(Enum.uniq(bHand)) == 2
+    end
+
+    def is_fullHouse(hand) do
+        is_threeOfKind(hand) &&
+        is_twoPair(hand)
+    end
+
     def normalizeHand(hand) do
         aceKingFn = fn
 			e1 when e1==0 or e1==1 -> e1+13
@@ -73,18 +73,35 @@ defmodule Poker do
         h1Base = Enum.sort_by(normalizeHand(h1Base), &(&1), :desc)
         h2Base = Enum.sort_by(normalizeHand(h2Base), &(&1), :desc)
 
-        ((h1Base > h2Base) && hand1)    ||
-        ((h2Base > h1Base) && hand2)    ||
-        ((hand1 > hand2) && hand1)      ||
-        hand2 
+        ((h1Base > h2Base) && hand1)    
+        || ((h2Base > h1Base) && hand2)    
+        || ((hand1 > hand2) && hand1)      
+        || hand2 
 
+    end
+
+    def getPairList(hand) do
+        hand
+        |> Enum.group_by(&(&1))
+        |> Enum.filter(fn {_, [_,_|_]} -> true; _ -> false end)
+        |> Enum.map(fn {x, _} -> x end)
+    end
+
+    def tie_pair(hand1, hand2) do 
+        h1Base = baseHand(hand1)
+        h2Base = baseHand(hand2)
+        pairList1 = Enum.sort_by(normalizeHand(h1Base), &(&1), :desc) |> getPairList
+        pairList2 = Enum.sort_by(normalizeHand(h2Base), &(&1), :desc) |> getPairList
+
+        ((pairList1 > pairList2) && pairList1) 
+        || ((pairList2 > pairList1) && pairList2)
+        || tie_highcard(hand1,hand2)
     end
 
     def find_n_OfKind(list,num) do	
 		normalizeHand(list)
 		|> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
 		|> Enum.find(fn {_, val} -> val == num  end) |> elem(0)
-
 	end
 
 	def tie_fourOfKind(hand1, hand2) do
@@ -119,12 +136,31 @@ defmodule Poker do
     def tieBreak(hand1, hand2, type) do
         case type do
             x when x in [0,4,5,8,9] -> tie_highcard(hand1, hand2)
-            #pair 1 -> 
-            #twopair 2 -> 
+            x when x in [1,3] -> tie_pair(hand1, hand2) 
             3 -> tie_threeOfKind(hand1, hand2)
             6 -> tie_highcard(hand1, hand2) #idk if this works
             7 -> tie_fourOfKind(hand1, hand2)
         end
+    end
+
+    def output(hand) do
+        remString = &(to_string(rem(&1,13)))
+        suitFunc = fn 
+            el when el in 1..13 -> to_string(el) <> "C"
+            el when el in 14..26 -> remString.(el) <> "D"
+            el when el in 27..39 -> remString.(el) <> "H"
+            el when el in 40..52 -> remString.(el) <> "S"
+            _ -> :error
+        end
+        kingFn = fn
+			e1 when e1==0 -> e1+13
+			e1->e1
+		end
+        hand 
+        |> Stream.map(suitFunc) 
+        |> Stream.map(kingFn) 
+        |> Enum.to_list
+        |> Enum.sort
     end
 
     def deal(intList) do
@@ -145,25 +181,5 @@ defmodule Poker do
         ((handOneType > handTwoType) && output(handOne) )   ||
         ((handTwoType > handOneType) && output(handTwo))    ||
         tieBreak(handOne, handTwo, handOneType) |> output
-    end
-
-    def output(hand) do
-        remString = &(to_string(rem(&1,13)))
-        eachFunc = fn 
-            el when el in 1..13 -> to_string(el) <> "C"
-            el when el in 14..26 -> remString.(el) <> "D"
-            el when el in 27..39 -> remString.(el) <> "H"
-            el when el in 40..52 -> remString.(el) <> "S"
-            _ -> :error
-        end
-        kingFn = fn
-			e1 when e1==0 -> e1+13
-			e1->e1
-		end
-        hand 
-        |> Stream.map(eachFunc) 
-        |> Stream.map(kingFn) 
-        |> Enum.to_list
-        |> Enum.sort
-    end
+    end  
 end
