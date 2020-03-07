999f7f182827276ccc4d68e8a62e5201228b6d41
Adam Sorrenti <adam.sorrenti@ryerson.ca>
2020-03-06 19:06:42 -0500

    tester.exs

--- tester.exs
+++ tester.exs
@@ -1,11 +1,29 @@
-list = [1, 2, 3, 4, 5]
+#unit tests
+defmodule Tester do
+    def dealTest(intList, winner) do
+        listWithIndexS = Stream.with_index(intList)
+        hands = listWithIndexS
+        |> Enum.reduce([[], []], fn ({x, i}, [evens, odds]) ->
+            case rem(i, 2) do
+                0 -> [evens ++ [x], odds]
+                _ -> [evens, odds ++ [x]]
+            end
+        end)
+        winner = case winner do
+            1 -> hd(hands)
+            2 -> hd(tl(hands))
+        end
+        Poker.deal(intList) == Poker.output(winner)
+    end  
+end
 
-list 
-|> Stream.with_index 
-|> Enum.reduce([[], []], fn ({x, i}, [evens, odds]) ->
-  case rem(i, 2) do
-    0 -> [evens ++ [x], odds]
-    _ -> [evens, odds ++ [x]]
-  end
-end)
-|> IO.inspect 
+games = [
+    {[1,2,3,4,5,6,7,8,9,10], 1},
+    {[40,1,38,12,48,35,43,30,15,28], 1},
+    {[40,1,38,12,48,35,43,30,15,29], 2}
+]
+
+testFunc = fn({intList,winnerInt}) ->
+    IO.puts(Tester.dealTest(intList, winnerInt))
+end
+Enum.each(games, testFunc)
